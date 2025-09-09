0c1449737a5a6dca98b24705587c960be0d859f1
yash <yashjha7463@gmail.com>
2025-09-09 15:46:31 +0530

    Cors fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/
 
 export const PIC_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId="
+export const MENU_API = "https://corsproxy.io/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId="
--- src/utils/useResdata.js
+++ src/utils/useResdata.js
@@ -21,7 +21,7 @@ const useResdata = () => {
         const fetchdata = async () => {
             try {
                 const resp = await fetch(
-                    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+                    "https://corsproxy.io/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
                 );
                 const jsondata = await resp.json();
                 const restaurants = extractRestaurants(jsondata);
